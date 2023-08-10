49af7f0f3f3dab3f9e4d821655feb2bcf2a677f3
Chao Gao <chao.gao@intel.com>
2023-08-10 23:29:28 +0800

    x86/virt/seamldr: Retrieve P-SEAMLDR information
    P-SEAMLDR returns its information e.g., version and supported features, in
    response to the SEAMLDR.INFO SEAMCALL.
    
    This information is useful for userspace. For example, the admin can decide
    which TDX module versions are compatible with the P-SEAMLDR according to
    the P-SEAMLDR version.
    
    Add and export seamldr_get_info() which retrieves P-SEAMLDR information by
    invoking SEAMLDR.INFO SEAMCALL in preparation for exposing P-SEAMLDR
    version and other necessary information to userspace.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>
    Tested-by: Farrah Chen <farrah.chen@intel.com>

---
+++ arch/x86/include/asm/seamldr.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_X86_SEAMLDR_H
+#define _ASM_X86_SEAMLDR_H
+
+#include <linux/types.h>
+
+struct seamldr_info {
+	u32	version;
+	u32	attributes;
+	u32	vendor_id;
+	u32	build_date;
+	u16	build_num;
+	u16	minor_version;
+	u16	major_version;
+	u16	update_version;
+	u8	reserved0[4];
+	u32	num_remaining_updates;
+	u8	reserved1[224];
+} __packed;
+
+#ifdef CONFIG_INTEL_TDX_MODULE_UPDATE
+const struct seamldr_info *seamldr_get_info(void);
+#else
+static inline const struct seamldr_info *seamldr_get_info(void) { return NULL; }
+#endif
+
+#endif
