81eae53dd81b0f3882d288ed47b4b2a2c56753db
Chao Gao <chao.gao@intel.com>
2023-08-31 10:17:46 +0800

    coco/tdx-host: Implement firmware upload sysfs ABI for TDX module updates
    Linux kernel supports two primary firmware update mechanisms:
      - request_firmware()
      - firmware upload (or fw_upload)
    
    The former is used by microcode updates, SEV firmware updates, etc. The
    latter is used by CXL and FPGA firmware updates.
    
    One key difference between them is: request_firmware() loads a named
    file from the filesystem where the filename is kernel-controlled, while
    fw_upload accepts firmware data directly from userspace.
    
    Use fw_upload for TDX module updates as loading a named file isn't
    suitable for TDX (see below for more reasons). Specifically, register
    TDX faux device with fw_upload framework to expose sysfs interfaces
    and implement operations to process data blobs supplied by userspace.
    
    Implementation notes:
    1. P-SEAMLDR processes the entire update at once rather than
       chunk-by-chunk, so .write() is called only once per update; so the
       offset should be always 0.
    2. An update completes synchronously within .write(), meaning
       .poll_complete() is only called after the update succeeds and so always
       returns success
    
    Why fw_upload instead of request_firmware()?
    ============================================
    The explicit file selection capabilities of fw_upload is preferred over
    the implicit file selection of request_firmware() for the following
    reasons:
    
    a. Intel distributes all versions of the TDX module, allowing admins to
    load any version rather than always defaulting to the latest. This
    flexibility is necessary because future extensions may require reverting to
    a previous version to clear fatal errors.
    
    b. Some module version series are platform-specific. For example, the 1.5.x
    series is for certain platform generations, while the 2.0.x series is
    intended for others.
    
    c. The update policy for TDX module updates is non-linear at times. The
    latest TDX module may not be compatible. For example, TDX module 1.5.x
    may be updated to 1.5.y but not to 1.5.y+1. This policy is documented
    separately in a file released along with each TDX module release.
    
    So, the default policy of "request_firmware()" of "always load latest", is
    not suitable for TDX. Userspace needs to deploy a more sophisticated policy
    check (e.g., latest may not be compatible), and there is potential
    operator choice to consider.
    
    Just have userspace pick rather than add kernel mechanism to change the
    default policy of request_firmware().
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>
    Reviewed-by: Tony Lindgren <tony.lindgren@linux.intel.com>
    Reviewed-by: Kai Huang <kai.huang@intel.com>
    Reviewed-by: Kiryl Shutsemau (Meta) <kas@kernel.org>
    ---
    v6:
     - Remove unused "-ENOSPC" error mapping—this series never returns it
    
    v5:
     - remove a tail comment [Yan]
     - remove is_vmalloc_addr() check [Dave]
     - use devm_add_action_or_reset() for deinit [Yilun]
     - remove global tdx_fwl [Yilun]
     - clarify request_firmware() doesn't take filename from userspace
       [Rick]
    
    v4:
     - make tdx_fwl static [Kai]
     - don't support update canceling [Yilun]
     - explain why seamldr_init() doesn't return an error [Kai]
     - bail out if TDX module updates are not supported [Kai]
     - name the firmware "tdx_module" instead of "seamldr_upload" [Cedric]
    
    v3:
     - clear "cancel_request" in the "prepare" phase [Binbin]
     - Don't fail the whole tdx-host device if seamldr_init() met an error
     [Yilun]
     - Add kdoc for seamldr_install_module() and verify that the input
       buffer is vmalloc'd. [Yilun]

--- drivers/virt/coco/tdx-host/tdx-host.c
+++ drivers/virt/coco/tdx-host/tdx-host.c
@@ -6,6 +6,7 @@
  */
 
 #include <linux/device/faux.h>
+#include <linux/firmware.h>
 #include <linux/module.h>
 #include <linux/mod_devicetable.h>
 #include <linux/sysfs.h>
@@ -98,7 +99,7 @@ static struct attribute *seamldr_attrs[] = {
 	NULL,
 };
 
-static bool seamldr_group_visible(struct kobject *kobj)
+static bool can_expose_seamldr(void)
 {
 	const struct tdx_sys_info *sysinfo = tdx_get_sysinfo();
 
@@ -106,6 +107,11 @@ static bool seamldr_group_visible(struct kobject *kobj)
 		return false;
 
 	return tdx_supports_runtime_update(sysinfo);
+}
+
+static bool seamldr_group_visible(struct kobject *kobj)
+{
+	return can_expose_seamldr();
 }
 
 DEFINE_SIMPLE_SYSFS_GROUP_VISIBLE(seamldr);
@@ -122,6 +128,95 @@ static const struct attribute_group *tdx_host_groups[] = {
 	NULL,
 };
 
+static enum fw_upload_err tdx_fw_prepare(struct fw_upload *fwl,
+					 const u8 *data, u32 size)
+{
+	return FW_UPLOAD_ERR_NONE;
+}
+
+static enum fw_upload_err tdx_fw_write(struct fw_upload *fwl, const u8 *data,
+				       u32 offset, u32 size, u32 *written)
+{
+	int ret;
+
+	/*
+	 * tdx_fw_write() always processes all data on the first call with
+	 * offset == 0. Since it never returns partial success (it either
+	 * succeeds completely or fails), there is no subsequent call with
+	 * non-zero offsets.
+	 */
+	WARN_ON_ONCE(offset);
+	ret = seamldr_install_module(data, size);
+	switch (ret) {
+	case 0:
+		*written = size;
+		return FW_UPLOAD_ERR_NONE;
+	case -EBUSY:
+		return FW_UPLOAD_ERR_BUSY;
+	case -EIO:
+		return FW_UPLOAD_ERR_HW_ERROR;
+	case -ENOMEM:
+		return FW_UPLOAD_ERR_RW_ERROR;
+	default:
+		return FW_UPLOAD_ERR_FW_INVALID;
+	}
+}
+
+static enum fw_upload_err tdx_fw_poll_complete(struct fw_upload *fwl)
+{
+	/*
+	 * TDX module updates are completed in the previous phase
+	 * (tdx_fw_write()). If any error occurred, the previous phase
+	 * would return an error code to abort the update process. In
+	 * other words, reaching this point means the update succeeded.
+	 */
+	return FW_UPLOAD_ERR_NONE;
+}
+
+/*
+ * TDX module updates cannot be cancelled. Provide a stub function since
+ * the firmware upload framework requires a .cancel operation.
+ */
+static void tdx_fw_cancel(struct fw_upload *fwl)
+{
+}
+
+static const struct fw_upload_ops tdx_fw_ops = {
+	.prepare	= tdx_fw_prepare,
+	.write		= tdx_fw_write,
+	.poll_complete	= tdx_fw_poll_complete,
+	.cancel		= tdx_fw_cancel,
+};
+
+static void seamldr_deinit(void *tdx_fwl)
+{
+	firmware_upload_unregister(tdx_fwl);
+}
+
+static int seamldr_init(struct device *dev)
+{
+	struct fw_upload *tdx_fwl;
+
+	if (!can_expose_seamldr())
+		return 0;
+
+	tdx_fwl = firmware_upload_register(THIS_MODULE, dev, "tdx_module",
+					   &tdx_fw_ops, NULL);
+	if (IS_ERR(tdx_fwl))
+		return PTR_ERR(tdx_fwl);
+
+	return devm_add_action_or_reset(dev, seamldr_deinit, tdx_fwl);
+}
+
+static int tdx_host_probe(struct faux_device *fdev)
+{
+	return seamldr_init(&fdev->dev);
+}
+
+static const struct faux_device_ops tdx_host_ops = {
+	.probe		= tdx_host_probe,
+};
+
 static struct faux_device *fdev;
 
 static int __init tdx_host_init(void)
@@ -129,7 +224,7 @@ static int __init tdx_host_init(void)
 	if (!x86_match_cpu(tdx_host_ids) || !tdx_get_sysinfo())
 		return -ENODEV;
 
-	fdev = faux_device_create_with_groups(KBUILD_MODNAME, NULL, NULL, tdx_host_groups);
+	fdev = faux_device_create_with_groups(KBUILD_MODNAME, NULL, &tdx_host_ops, tdx_host_groups);
 	if (!fdev)
 		return -ENODEV;
 
