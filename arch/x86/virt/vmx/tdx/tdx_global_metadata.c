1e79c19ea04970904c039d6aaee9f10dce80d4d1
Chao Gao <chao.gao@intel.com>
2024-06-04 09:40:48 +0800

    x86/virt/seamldr: Shut down the current TDX module
    The first step of TDX module updates is shutting down the current TDX
    Module. This step also packs state information that needs to be
    preserved across updates as handoff data, which will be consumed by the
    updated module. The handoff data is stored internally in the SEAM range
    and is hidden from the kernel.
    
    To ensure a successful update, the new module must be able to consume
    the handoff data generated by the old module. Since handoff data layout
    may change between modules, the handoff data is versioned. Each module
    has a native handoff version and provides backward support for several
    older versions.
    
    The complete handoff versioning protocol is complex as it supports both
    module upgrades and downgrades. See details in Intel® Trust Domain
    Extensions (Intel® TDX) Module Base Architecture Specification, Chapter
    "Handoff Versioning".
    
    Ideally, the kernel needs to retrieve the handoff versions supported by
    the current module and the new module and select a version supported by
    both. But, since this implementation chooses to only support module
    upgrades, simply request the current module to generate handoff data
    using its highest supported version, expecting that the new module will
    likely support it.
    
    Note that only one CPU needs to call the TDX module's shutdown API.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>
    Reviewed-by: Tony Lindgren <tony.lindgren@linux.intel.com>
    Reviewed-by: Xu Yilun <yilun.xu@linux.intel.com>
    Reviewed-by: Kai Huang <kai.huang@intel.com>
    Reviewed-by: Kiryl Shutsemau (Meta) <kas@kernel.org>
    ---
    v5:
     - Massage changelog [Kai]
     - Avoid "refers to the global copy while populating the tdx_sys_info
       passed as a pointer" [Rick/Yilun]
    
    v4:
     - skip the whole handoff metadata if runtime updates are not supported
       [Yilun]
    v3:
     - remove autogeneration stuff in the changelog
    v2:
     - add a comment about how handoff version is chosen.
     - remove the first !ret in get_tdx_sys_info_handoff() as we edited the
       auto-generated code anyway
     - remove !! when determining whether a CPU is the primary one
     - remove unnecessary if-break nesting in TDP_SHUTDOWN

--- arch/x86/virt/vmx/tdx/tdx_global_metadata.c
+++ arch/x86/virt/vmx/tdx/tdx_global_metadata.c
@@ -100,6 +100,19 @@ static int get_tdx_sys_info_td_conf(struct tdx_sys_info_td_conf *sysinfo_td_conf
 	return ret;
 }
 
+static int get_tdx_sys_info_handoff(struct tdx_sys_info_handoff *sysinfo_handoff)
+{
+	int ret;
+	u64 val;
+
+	ret = read_sys_metadata_field(0x8900000100000000, &val);
+	if (ret)
+		return ret;
+
+	sysinfo_handoff->module_hv = val;
+	return 0;
+}
+
 static int get_tdx_sys_info(struct tdx_sys_info *sysinfo)
 {
 	int ret = 0;
@@ -115,6 +128,13 @@ static int get_tdx_sys_info(struct tdx_sys_info *sysinfo)
 	ret = ret ?: get_tdx_sys_info_tdmr(&sysinfo->tdmr);
 	ret = ret ?: get_tdx_sys_info_td_ctrl(&sysinfo->td_ctrl);
 	ret = ret ?: get_tdx_sys_info_td_conf(&sysinfo->td_conf);
+
+	/*
+	 * Don't treat a module that doesn't support update as a failure.
+	 * Only read the metadata optionally.
+	 */
+	if (tdx_supports_runtime_update(sysinfo))
+		ret = ret ?: get_tdx_sys_info_handoff(&sysinfo->handoff);
 
 	return ret;
 }
