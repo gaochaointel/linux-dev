c3c250a8498dab84674ca82eae4b3fbbde65a095
Chao Gao <chao.gao@intel.com>
2025-02-27 00:17:59 -0800

    x86/virt/tdx: Restore TDX Module state
    TDX Module state was packed as handoff data during module shutdown. After
    per-CPU initialization, the new module can restore TDX Module state from
    handoff data to preserve running TDs.
    
    Once the restoration is done, the TDX Module update is complete, which
    means the new module is ready to handle requests from the host and guests.
    
    Implement the new TDH.SYS.UPDATE SEAMCALL to restore TDX Module state
    and invoke it for one CPU.
    
    Note that Intel® Trust Domain Extensions (Intel® TDX) Module Base
    Architecture Specification, Revision 348549-007, Chapter 4.5.5 states:
    
      If TDH.SYS.UPDATE returns an error, then the host VMM can continue
      with the non-update sequence (TDH.SYS.CONFIG, 15 TDH.SYS.KEY.CONFIG
      etc.). In this case all existing TDs are lost. Alternatively, the host
      VMM can request the P-SEAMLDR to update to another TDX Module. If that
      update is successful, existing TDs are preserved
    
    The two alternative error handling approaches are not implemented due to
    their complexity and unclear benefits.
    
    Also note that the location and the format of handoff data is defined by
    the TDX Module. The new module knows where to get handoff data and how
    to parse it. The kernel doesn't need to provide its location, format etc.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>
    Reviewed-by: Tony Lindgren <tony.lindgren@linux.intel.com>
    ---
    v3:
     - use seamcall_prerr() rather than raw seamcall() [Binbin]
     - use pr_err() to print error message [Binbin]

--- arch/x86/virt/vmx/tdx/seamldr.c
+++ arch/x86/virt/vmx/tdx/seamldr.c
@@ -201,6 +201,7 @@ enum tdp_state {
 	TDP_SHUTDOWN,
 	TDP_CPU_INSTALL,
 	TDP_CPU_INIT,
+	TDP_RUN_UPDATE,
 	TDP_DONE,
 };
 
@@ -263,6 +264,10 @@ static int do_seamldr_install_module(void *seamldr_params)
 				break;
 			case TDP_CPU_INIT:
 				ret = tdx_cpu_enable();
+				break;
+			case TDP_RUN_UPDATE:
+				if (primary)
+					ret = tdx_module_run_update();
 				break;
 			default:
 				break;
