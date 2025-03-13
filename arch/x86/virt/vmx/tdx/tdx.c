890ac69dda17af326868f9a760e1daa5381f7443
Chao Gao <chao.gao@intel.com>
2025-03-13 02:05:53 -0700

    x86/virt/tdx: Reset software states during TDX module shutdown
    The TDX module requires a one-time global initialization (TDH.SYS.INIT) and
    per-CPU initialization (TDH.SYS.LP.INIT) before use. These initializations
    are guarded by software flags to prevent repetition.
    
    After TDX module updates, the new TDX module requires the same global and
    per-CPU initializations, but the existing software flags prevent
    re-initialization.
    
    Reset all software flags guarding the initialization flows to allow the
    global and per-CPU initializations to be triggered again after updates.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>
    Reviewed-by: Tony Lindgren <tony.lindgren@linux.intel.com>
    Reviewed-by: Kai Huang <kai.huang@intel.com>
    ---
    v5:
     - add a comment to clarify why state access doesn't require holding a
       lock. [Kai]

--- arch/x86/virt/vmx/tdx/tdx.c
+++ arch/x86/virt/vmx/tdx/tdx.c
@@ -55,6 +55,8 @@ static struct tdmr_info_list tdx_tdmr_list;
 
 static enum tdx_module_status_t tdx_module_status;
 static DEFINE_MUTEX(tdx_module_lock);
+static bool sysinit_done;
+static int sysinit_ret;
 
 /* All TDX-usable memory regions.  Protected by mem_hotplug_lock. */
 static LIST_HEAD(tdx_memlist);
@@ -70,8 +72,6 @@ static int try_init_module_global(void)
 {
 	struct tdx_module_args args = {};
 	static DEFINE_RAW_SPINLOCK(sysinit_lock);
-	static bool sysinit_done;
-	static int sysinit_ret;
 
 	lockdep_assert_irqs_disabled();
 
@@ -1179,6 +1179,7 @@ EXPORT_SYMBOL_FOR_KVM(tdx_enable);
 int tdx_module_shutdown(void)
 {
 	struct tdx_module_args args = {};
+	int ret, cpu;
 
 	/*
 	 * Shut down the TDX module and prepare handoff data for the next
@@ -1188,7 +1189,22 @@ int tdx_module_shutdown(void)
 	 * modules as new modules likely have higher handoff version.
 	 */
 	args.rcx = tdx_sysinfo.handoff.module_hv;
-	return seamcall_prerr(TDH_SYS_SHUTDOWN, &args);
+	ret = seamcall_prerr(TDH_SYS_SHUTDOWN, &args);
+	if (ret)
+		return ret;
+
+	tdx_module_status = TDX_MODULE_UNINITIALIZED;
+	sysinit_done = false;
+	sysinit_ret = 0;
+
+	/*
+	 * By reaching here CPUHP is disabled and all present CPUs
+	 * are online. It's safe to just loop all online CPUs and
+	 * reset the per-cpu flag.
+	 */
+	for_each_online_cpu(cpu)
+		per_cpu(tdx_lp_initialized, cpu) = false;
+	return 0;
 }
 
 static bool is_pamt_page(unsigned long phys)
