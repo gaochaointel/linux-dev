370d28b3a8d5a4a0f3b4f25a8636f99e35cbac2e
Chao Gao <chao.gao@intel.com>
2025-09-29 00:26:01 -0700

    x86/virt/tdx: Enable TDX Module runtime updates
    All pieces of TDX Module runtime updates are in place. Enable it if it
    is supported.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -32,6 +32,10 @@
 #define TDX_SUCCESS		0ULL
 #define TDX_RND_NO_ENTROPY	0x8000020300000000ULL
 
+/* Bit definitions of TDX_FEATURES0 metadata field */
+#define TDX_FEATURES0_TD_PRESERVING	BIT(1)
+#define TDX_FEATURES0_NO_RBP_MOD	BIT(18)
+
 /* P-SEAMLDR SEAMCALL leaf function error codes */
 #define SEAMLDR_RND_NO_ENTROPY	0x8000000000030001ULL
 
@@ -109,7 +113,7 @@ const struct tdx_sys_info *tdx_get_sysinfo(void);
 
 static inline bool tdx_supports_runtime_update(const struct tdx_sys_info *sysinfo)
 {
-	return false; /* To be enabled when kernel is ready */
+	return sysinfo->features.tdx_features0 & TDX_FEATURES0_TD_PRESERVING;
 }
 
 int tdx_guest_keyid_alloc(void);
