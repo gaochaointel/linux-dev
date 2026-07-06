d18eee873a4782b0c6f57501e50df4fdbb7830ab
Chao Gao <chao.gao@intel.com>
2026-07-05 18:42:20 -0700

    KVM: VMX: Define APIC timer virtualization VMCS fields and control bit
    Add the architectural definitions for APIC timer virtualization:
    
      - VMX_FEATURE_APIC_TIMER_VIRT: tertiary processor-based control bit 8
      - TERTIARY_EXEC_APIC_TIMER_VIRT: the corresponding control macro
      - APIC_TIMER_VIRT_VECTOR (0x000a): 16-bit vector for timer injection
      - APIC_TIMER_VIRT_DEADLINE (0x204e): 64-bit virtual TSC deadline
      - APIC_TIMER_PHYS_DEADLINE (0x2830): 64-bit physical TSC deadline
    
    When the tertiary control is set and the guest writes MSR_IA32_TSC_DEADLINE
    (with the MSR bitmap clear for that MSR), the processor stores the value
    in APIC_TIMER_VIRT_DEADLINE and delivers the timer interrupt via the
    virtual interrupt delivery path when the deadline expires, without causing
    a VM-exit.
    
    No functional change.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>

--- arch/x86/include/asm/vmxfeatures.h
+++ arch/x86/include/asm/vmxfeatures.h
@@ -90,4 +90,5 @@
 
 /* Tertiary Processor-Based VM-Execution Controls, word 3 */
 #define VMX_FEATURE_IPI_VIRT		( 3*32+  4) /* "ipi_virt" Enable IPI virtualization */
+#define VMX_FEATURE_APIC_TIMER_VIRT	( 3*32+  8) /* "apic_timer_virt" APIC timer virtualization */
 #endif /* _ASM_X86_VMXFEATURES_H */
